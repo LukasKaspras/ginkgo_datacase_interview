17d1270b6f74b5890036bccf4fc40c3fd611bf21
Lukas Kaspras <46094685+LukasKaspras@users.noreply.github.com>
2021-05-21 19:59:00 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A short exploration and modelling of sensor data in order to predict and warn about upcoming device failures.',
+    author='Lukas Kaspras',
+    license='',
+)
